0c04fc7def275785ada1046e12a5fbb4129b3b25
mihai-dima <mihai.dima@info.uaic.ro>
2022-03-29 16:29:31 +0300

    First commit

---
+++ movieRecomander/movieRecomander/src/test/java/com/movieRecomander/movieRecomander/MovieRecomanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieRecomander.movieRecomander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecomanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
